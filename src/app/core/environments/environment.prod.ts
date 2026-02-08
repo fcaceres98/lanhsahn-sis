d8d319e0232562f4024f5ab69bff22431417b246
fcaceres98 <fausto.fernandez1986@gmail.com>
2026-02-08 09:55:40 -0600

    CONFIGURED FOR PRODUCTION ADDING PUBLIC URL

--- src/app/core/environments/environment.prod.ts
+++ src/app/core/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // Environment configuration for production
 export const environment = {
   production: true,
-  apiUrl: 'https://lanhsahn-sis-backend:8080/',
+  apiUrl: 'https://lanhsahn-sis-backend-production.up.railway.app',
   appName: 'lanhsahn-sis',
   logLevel: 'error'
 };
